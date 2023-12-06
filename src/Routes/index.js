dffe8db2230d3b659db0174b0a2e08a25ec5b598
thigasmonteiro <thiagodepaulamonteiro@gmail.com>
2023-12-06 15:58:45 -0300

    estilizacao tabbar

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -9,25 +9,68 @@ import Search from "../Pages/Search";
 import Header from "../components/Header";
 import LibraryCatalog from "../Pages/Collection";
 
+import { Ionicons } from '@expo/vector-icons'
+
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function HomeTabs(){
     
     return(
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarStyle:{
+                    position:'absolute',
+                    backgroundColor:'orange',
+                    borderTopWidth:0,
+
+                    bottom:14,
+                    left:14,
+                    right:14,
+                    elevation:0,
+                    borderRadius:4,
+                    height:60,
+                }
+            }}
+        >
 
             <Tab.Screen
                 name="Home" component={Home}
-                options={{headerShown:false}}
+                options={{headerShown:false,
+                    headerShown:false,
+                    tabBarIcon: ({color, size, focused}) => {
+                        if (focused){
+                            return <Ionicons name="home" size={size} color={color} /> 
+                        }
+                        return <Ionicons name="home-outline" size={size} color={color} />
+                    }
+                }}
+               
+
+                
+
             />
              <Tab.Screen
                 name="Search" component={Search}
-                options={{headerShown:false}}
+                options={{headerShown:false,
+                    tabBarIcon: ({color, size, focused}) => {
+                        if (focused){
+                            return <Ionicons name="search" size={size} color={color} /> 
+                        }
+                        return <Ionicons name="search-outline" size={size} color={color} />
+                    }
+                }}
             />
             <Tab.Screen
                 name="Librarycatalog" component={LibraryCatalog}
-                options={{headerShown:false}}
+                options={{headerShown:false,
+                    tabBarIcon: ({color, size, focused}) => {
+                        if (focused){
+                            return <Ionicons name="person" size={size} color={color} /> 
+                        }
+                        return <Ionicons name="person-outline" size={size} color={color} />
+                    }}}
             />
         
         </Tab.Navigator>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,7 +9,7 @@ export default function Header(){
                 <View style={styles.iconContainer}>
                     <AntDesign name='bars' size={30} color={'white'}/>
                 </View>
-                <Text style={styles.title}> Biblioteca Pública Paraná </Text>
+                <Text style={styles.title}> Biblioteca</Text>
                 <View style={styles.logoContainer}>
                     <Image source={{uri: 'https://media.glassdoor.com/sql/2493747/biblioteca-p%C3%BAblica-do-paran%C3%A1-squarelogo-1555303572425.png' }} 
                     style={styles.logo}  />
